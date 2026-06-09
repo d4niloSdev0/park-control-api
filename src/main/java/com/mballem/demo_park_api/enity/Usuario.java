e0c626a951d1d96ef21b7af1a054a69844669fa0
Danilo Santos <d4nilo.nilo10@gmail.com>
2026-06-09 19:22:46 -0300

    Implementa busca por usuario por ID

--- src/main/java/com/mballem/demo_park_api/enity/Usuario.java
+++ src/main/java/com/mballem/demo_park_api/enity/Usuario.java
@@ -16,8 +16,8 @@ public class Usuario implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
-    @Column(name = "usarname", nullable = false, unique = true, length = 100)
-    private String usarname;
+    @Column(name = "username", nullable = false, unique = true, length = 100)
+    private String username;
     @Column(name = "password", nullable = false, length = 200)
     private String password;
 
--- src/main/java/com/mballem/demo_park_api/web/controller/UsuarioController.java
+++ src/main/java/com/mballem/demo_park_api/web/controller/UsuarioController.java
@@ -5,21 +5,23 @@ import com.mballem.demo_park_api.service.UsuarioService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("api/v1/usuarios")
-public class UsuarioController {
+public class git UsuarioController {
 
     private final UsuarioService usuarioService;
 
-    @PostMapping
+    @PostMapping("/create")
     public ResponseEntity<Usuario> create(@RequestBody Usuario usuario) {
         Usuario user = usuarioService.salvar(usuario);
         return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<Usuario> getById(@PathVariable Long id) {
+        Usuario user = usuarioService.buscarPorId(id);
+        return ResponseEntity.ok(user);
     }
 }
