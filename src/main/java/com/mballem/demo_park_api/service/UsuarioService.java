e6fff628ca36f2d95dbf35a93bd1387de2ebaf7d
Danilo Santos <d4nilo.nilo10@gmail.com>
2026-06-11 17:19:04 -0300

    Feat: endpoint de listagem de usuario

--- src/main/java/com/mballem/demo_park_api/service/UsuarioService.java
+++ src/main/java/com/mballem/demo_park_api/service/UsuarioService.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @Service
 public class UsuarioService {
@@ -29,5 +31,9 @@ public class UsuarioService {
         user.setPassword(password);
         return user;
 
+    }
+    @Transactional(readOnly = true)
+    public List<Usuario> buscarTodos() {
+        return usuarioRepository.findAll();
     }
 }
